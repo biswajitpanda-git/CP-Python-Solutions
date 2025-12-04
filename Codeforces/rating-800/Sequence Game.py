e57f2c21d99545365e411556b8ad31c498d622c7
Biswajit Panda <biswajit.panda200b@gmail.com>
2025-12-04 20:28:52 +0530

    Solved Sequence Game

---
+++ Codeforces/rating-800/Sequence Game.py
@@ -0,0 +1,23 @@
+import sys 
+
+data = sys.stdin.read().split()
+index = 0
+
+t = int(data[index])
+index += 1
+
+for _ in range(t):
+    n = int(data[index])
+    index += 1
+    b = list(map(int, data[index : index+n]))
+    index += n
+    a =list()
+    a.append(b[0])
+    for i in range(1, n):
+        if b[i] >= b[i-1]:
+            a.append(b[i])
+        else:
+            a.append(b[i])
+            a.append(b[i])
+    print(len(a))
+    print(*a)
